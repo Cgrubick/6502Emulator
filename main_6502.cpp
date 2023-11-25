e99cb83b2b25a566f8a8265bc97aa8eec2101f3a
cgrubick <cgrubick@gmail.com>
2023-11-25 16:36:57 -0500

    added JSR and LDA ZP X and LDA ZP

--- main_6502.cpp
+++ main_6502.cpp
@@ -27,6 +27,12 @@ struct MEM {
     Byte& operator[](u32 addr){
         return Data[addr];
     }
+
+    void WriteWord(Word val, u32 addr, u32 cycles){
+        Data[addr] = val & 0xFF; //LSB
+        Data[addr+1] = (val >> 8); //MSB
+        cycles -= 2;
+    }
     
 };
 
@@ -61,10 +67,39 @@ struct CPU {
         return data;
 
     }
+
+    Byte ReadByte(u32 cycles,Byte addr, MEM &mem){
+        // Wont Inc PC since we just are reading and not executing an instruction
+        Byte data = mem[addr];
+        cycles--;
+        return data;
+    }
+
+    Word FetchWord(u32 cycles,MEM &mem){
+
+        // Little Endian for 6502
+        Word data = mem[PC];
+        PC++;
+        cycles--;
+
+        data |= (mem[PC] << 8);  //adding on the most significant bits
+        PC++;
+        cycles--;
+
+        return data;
+    }
+
     // OPCODES
     static constexpr Byte
-        INSTR_LDA_IMMEDIATE = 0xA9;
+        INSTR_LDA_IMMEDIATE = 0xA9,
+        INSTR_LDA_ZEROPAGE  = 0xA5,
+        INSTR_LDA_ZEROPAGEX = 0xB5,
+        INSTR_JSR = 0x20;
 
+    void LDASetFlagStatus(){
+        Z = (ACC == 0);
+        N = (ACC & 0b10000000) > 0;
+    }
 
     void Execute(u32 cycles, MEM &mem){
         while(cycles > 0){
@@ -72,12 +107,37 @@ struct CPU {
             Byte instr = Fetch8bits(cycles,mem);
 
             switch(instr){
+
                 case INSTR_LDA_IMMEDIATE:{
                     Byte val = Fetch8bits(cycles, mem);
                     ACC = val;
-                    Z = (ACC == 0);
-                    N = (ACC & 0b10000000) > 0; // checking if ACC has the neg sign, if greater than 0, if neg, then flag set
+                    LDASetFlagStatus(); // checking if ACC has the neg sign, if greater than 0, if neg, then flag set
                 }break;
+
+                case INSTR_LDA_ZEROPAGE:{
+                    Byte ZP_addr = Fetch8bits(cycles, mem); //1 clk cycle
+                    ACC = ReadByte(cycles, ZP_addr, mem); // 1 clk cycle
+                    LDASetFlagStatus(); 
+
+                }break;
+
+                case INSTR_LDA_ZEROPAGEX:{
+                    Byte ZP_addr = Fetch8bits(cycles, mem); //1 clk cycle
+                    ZP_addr += X; 
+                    cycles--;
+                    ACC = ReadByte(cycles, ZP_addr, mem); // 1 clk cycle
+                    LDASetFlagStatus(); 
+
+                }break;
+
+                case INSTR_JSR:{
+                    Word routine_addr = FetchWord(cycles, mem);
+                    mem.WriteWord(PC-1, routine_addr, cycles);
+                    PC = routine_addr;
+                    SP++;
+                    cycles--;
+                }break;
+
                 default:{
                     printf("%d: Invalid Instruction\n",instr);
                 }break;
@@ -87,14 +147,17 @@ struct CPU {
 };
 
 int main(){
-    u32 clk_cycles = 2;
+    u32 clk_cycles = 9;
     MEM mem;
     CPU cpu;
     cpu.Reset(mem);
 
     // hardcoded program
-    mem[0xFFFC] = CPU::INSTR_LDA_IMMEDIATE;
-    mem[0xFFFD] = 0x42;
+    mem[0xFFFC] = CPU::INSTR_JSR; // 6 cycles
+    mem[0xFFFD] = 0x42; 
+    mem[0xFFFE] = 0x42;
+    mem[0x4242] = CPU::INSTR_LDA_IMMEDIATE; // 3 cycles
+    mem[0x4243] = 0x84;
 
 
     cpu.Execute(clk_cycles, mem);
